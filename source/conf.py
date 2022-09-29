6bb030621505e20368dcd49e878fb11846ab1fb5
Cameron Smith <camrn86@gmail.com>
2022-09-29 01:25:30 +0000

    update sphinx configuration

--- source/conf.py
+++ source/conf.py
@@ -18,37 +18,51 @@
 # -- Project information -----------------------------------------------------
 master_doc = 'index'
 
-project = 'Single cell analysis'
-copyright = '2022, Cameron Smith'
-author = 'Cameron Smith'
+project = "Single cell analysis"
+copyright = "2022, Cameron Smith"
+author = "Cameron Smith"
 
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
-    'nbsphinx',
-    'sphinxcontrib.bibtex',        # for bibliographic references
-    'sphinx_copybutton',           # for adding “copy to clipboard” buttons to all text/code boxes | commented due to multiple scrollbar issue https://github.com/cameronraysmith/sc/issues/1
-    'sphinxcontrib.rsvgconverter', # for SVG->PDF conversion in LaTeX output
-    # 'sphinx_last_updated_by_git',  # get "last updated" from Git
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+    "sphinxcontrib.bibtex",        # for bibliographic references
+    "sphinx_copybutton",           # for adding “copy to clipboard” buttons to all text/code boxes | commented due to multiple scrollbar issue https://github.com/cameronraysmith/sc/issues/1
+    "sphinxcontrib.rsvgconverter", # for SVG->PDF conversion in LaTeX output
+    "sphinx_design",
+    "sphinx_inline_tabs",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+# templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['*.py','*.md', '*/*.py','*/*.md']
-
+nbsphinx_execute = "never"
 
 pygments_style = "sphinx"
 pygments_dark_style = "monokai"
 
 html_theme_options = {
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/cameronraysmith/sc",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
     "source_repository": "https://github.com/cameronraysmith/sc/",
     "source_branch": "master",
     "source_directory": "source/",
